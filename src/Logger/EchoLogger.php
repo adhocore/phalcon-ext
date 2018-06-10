fe2cd462dd33aaabe594d447a34872ca4874598d
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-10 09:53:22 +0700

    feat: logger extension

---
+++ src/Logger/EchoLogger.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PhalconExt\Logger;
+
+use Phalcon\Logger\Adapter as LoggerAdapter;
+use Phalcon\Logger\Formatter\Line as LineFormatter;
+
+/**
+ * Echo logger targeted for CLI environment.
+ */
+class EchoLogger extends LoggerAdapter
+{
+    /** @var array */
+    protected $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+
+        if ($config['level'] ?? null) {
+            $this->setLogLevel($config['level']);
+        }
+    }
+
+    /**
+     * Echoes the log message as it is in CLI!
+     */
+    protected function logInternal(string $message, int $type, int $time, array $context): void
+    {
+        echo $this->getFormatter()->format($message, $type, $time, $context);
+    }
+
+    /**
+     * Gets a plain line formatter with configured format.
+     *
+     * @return LineFormatter
+     */
+    public function getFormatter(): LineFormatter
+    {
+        if (!$this->_formatter) {
+            $this->_formatter = new LineFormatter(
+                $this->config['format'] ?? '[%type%][%date%] %message%',
+                $this->config['date_format'] ?? 'Y-m-d H:i:s'
+            );
+        }
+
+        return $this->_formatter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close()
+    {
+        return true;
+    }
+}
