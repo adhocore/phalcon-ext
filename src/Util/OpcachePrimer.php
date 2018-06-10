66abc93a46f993762e7c6074bf5393180d9a266e
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-10 09:55:34 +0700

    feat: util opcache primer

---
+++ src/Util/OpcachePrimer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PhalconExt\Util;
+
+class OpcachePrimer
+{
+    public function prime(array $paths): int
+    {
+        if (!\function_exists('opcache_compile_file')) {
+            return 0;
+        }
+
+        $cached = 0;
+
+        foreach ($paths as $path) {
+            if (false === $path = \realpath($path)) {
+                continue;
+            }
+
+            $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
+
+            foreach ($this->filter($iterator) as $file) {
+                \opcache_compile_file($file->getRealPath()) && $cached++;
+            }
+        }
+
+        return $cached;
+    }
+
+    protected function filter(\RecursiveIteratorIterator $iterator): \FilterIterator
+    {
+        return new class($iterator) extends \FilterIterator
+        {
+            public function accept(): bool
+            {
+                return $this->getInnerIterator()->current()->getExtension() === 'php';
+            }
+        };
+    }
+}
