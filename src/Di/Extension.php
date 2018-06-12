895f8e6a704dfb9dc771037abfa2ff76cdafd13f
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-12 20:08:26 +0700

    style: spacing

--- src/Di/Extension.php
+++ src/Di/Extension.php
@@ -23,10 +23,15 @@ trait Extension
 
     // Implemented by \Phalcon\Di.
     abstract public function set($service, $definition, $shared);
+
     abstract public function setShared($service, $definition);
+
     abstract public function get($service, $parameters);
+
     abstract public function getShared($service);
+
     abstract public function has($service);
+
     abstract public function remove($service);
 
     /**
