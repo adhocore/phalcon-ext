7fb727084643ba873cf918a35e54ea2769a09d79
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-17 19:24:05 +0700

    test(http): cors middleware

---
+++ tests/Http/Middleware/CorsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PhalconExt\Test\Http\Middleware;
+
+use Phalcon\Http\Request;
+use Phalcon\Http\Response;
+use PhalconExt\Http\Middleware\Cors;
+use Phalcon\Db\Adapter;
+use PhalconExt\Di\FactoryDefault;
+use PhalconExt\Di\ProvidesDi;
+use PhalconExt\Test\WebTestCase;
+
+class CorsTest extends WebTestCase
+{
+    protected $corsMw;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // $this->configure(['cors' => ['']]);
+
+        $this->app->before($this->corsMw = new Cors);
+    }
+
+    public function test_cors_headers()
+    {
+        $headers = ['Access-Control-Request-Method' => 'GET', 'Origin' => 'http://127.0.0.1:1234'];
+
+        // Preflight request (OPTIONS)
+        $this->doRequest('OPTIONS /corsheader', [], $headers)
+            ->assertResponseOk()
+            ->assertHeaderKeys(['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'])
+            ->assertHeaderKeys(['Access-Control-Max-Age', 'Access-Control-Allow-Methods']);
+
+        unset($headers['Access-Control-Request-Method']);
+
+        // Normal request (GET)
+        $this->doRequest('/corsheader', [], $headers)
+            ->assertResponseOk()
+            ->assertResponseJson()
+            ->assertNotHeaderKeys(['Access-Control-Max-Age', 'Access-Control-Allow-Methods']);
+    }
+}
