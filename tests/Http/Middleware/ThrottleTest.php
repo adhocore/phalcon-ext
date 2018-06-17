3625863bb2875fdd4cdd0b61c0b86c5560994280
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-17 17:36:31 +0700

    test(http): throttle middleware

---
+++ tests/Http/Middleware/ThrottleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PhalconExt\Test\Http\Middleware;
+
+use Phalcon\Http\Request;
+use Phalcon\Http\Response;
+use PhalconExt\Http\Middleware\Throttle;
+use Phalcon\Db\Adapter;
+use PhalconExt\Di\FactoryDefault;
+use PhalconExt\Di\ProvidesDi;
+use PhalconExt\Test\WebTestCase;
+
+class ThrottleTest extends WebTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Allow max 1 hits
+        $this->configure(['throttle' => ['maxHits' => [1 => 1], 'checkUserAgent' => true]]);
+
+        $this->app->before(new Throttle);
+    }
+
+    public function test_throttles()
+    {
+        // req #1 allowed
+        $this->doRequest('/')->assertResponseOk();
+
+        // req #2 not allowed
+        $this->doRequest('/')->assertResponseNotOk()->assertStatusCode(429);
+
+        $this->assertResponseContains('Too many requests');
+    }
+}
