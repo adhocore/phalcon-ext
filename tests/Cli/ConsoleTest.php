1c59a414e8390d8edd448611a22b95ad4db6529c
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-07-07 17:02:14 +0700

    test: cli/console

---
+++ tests/Cli/ConsoleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PhalconExt\Test\Cli;
+
+use PhalconExt\Cli\Console;
+use PhalconExt\Cli\Extension as CliExtension;
+use PhalconExt\Cli\MiddlewareTrait;
+use PhalconExt\Di\ProvidesDi;
+use PhalconExt\Test\ConsoleTestCase;
+
+class ConsoleTest extends ConsoleTestCase
+{
+    public function test()
+    {
+        $this->assertInstanceOf(Console::class, $this->app);
+
+        $this->assertNotEmpty($traits = class_uses_deep($this->app, false));
+
+        $this->assertArrayHasKey(ProvidesDi::class, $traits);
+        $this->assertArrayHasKey(CliExtension::class, $traits);
+        $this->assertArrayHasKey(MiddlewareTrait::class, $traits);
+
+        $this->assertTrue(method_exists($this->app, 'doHandle'));
+        $this->assertTrue(method_exists($this->app, 'bindEvents'));
+        $this->assertTrue(method_exists($this->app, 'middleware'));
+    }
+}
