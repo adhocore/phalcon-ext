4e91e1a37ae8907489714091f08416eaa14aeec1
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-07-07 17:05:04 +0700

    test: factory middleware

---
+++ tests/Cli/Middleware/FactoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PhalconExt\Test\Cli\Middleware;
+
+use PhalconExt\Test\ConsoleTestCase;
+
+class FactoryTest extends ConsoleTestCase
+{
+    public function test_loads_command()
+    {
+        $this->assertFalse($this->di()->has('command'));
+
+        $task = $this->app->handle(['test', 'some', '-s', rand(1, 100)]);
+
+        $this->assertTrue($this->di()->has('command'), 'Should be loaded by Factory Middleware');
+    }
+
+    public function test_app_help()
+    {
+        $this->app->handle(['test', '--help']);
+
+        $this->assertTaskOutputs('test, version 0.0.1-test');
+        $this->assertTaskOutputs('Commands:');
+        $this->assertTaskOutputs('MainTask::main');
+    }
+
+    public function test_command_help()
+    {
+        $this->app->handle(['test', 'main', '--help']);
+
+        $this->assertTaskOutputs('Command main, version 0.0.1-test');
+        $this->assertTaskOutputs('MainTask::main');
+        $this->assertTaskOutputs('main [OPTIONS...] [ARGUMENTS...]');
+        $this->assertTaskOutputs('[dummy]');
+        $this->assertTaskOutputs('[-d|--doo]');
+    }
+
+    public function test_version()
+    {
+        $this->app->handle(['test', '--version']);
+
+        $this->assertTaskOutputs('0.0.1-test');
+    }
+}
